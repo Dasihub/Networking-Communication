64bfb3c1b1484b840bbd4181809720a11b97215b
Dasihub <dasihub02@gmail.com>
2022-05-30 12:09:28 +0600

    edit config babelrc and postcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 
 const App: React.FC = () => {
-    const [value, setValue] = React.useState<string>('')
-
-    const change = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(pre => pre = e.target.value)
-    }
 
     return (
         <>
-            <input
-                value={value}
-                onChange={change}
-            />
-            <div>{value}</div>
         </>
     )
 }
